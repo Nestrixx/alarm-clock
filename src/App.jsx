a0e40065f473a515692e6e367567d3eeb5030e4b
crimtin <christianmck@hotmail.com>
2022-04-26 23:47:41 -0500

    removed defult code added styles. displays background video, time, and sets alarms.

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import "./App.css";
+
+import BackgroundVideo from "./BackgroundVideo.mp4";
+import WakeUp from "./WakeUp.mp4";
+import { useState } from "react";
+import { addMinutes } from "date-fns";
+import { setSeconds } from "date-fns";
+import { differenceInMinutes } from "date-fns";
+
+function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [alarmTime, setAlarmTime] = useState("");
+
+  const updateTime = () => {
+    const localeTime = new Date().toLocaleTimeString();
+
+    setTime(localeTime);
+  };
+
+  const alarmClock = () => {
+    if (time === "2:47:0") {
+      return (
+        <video autoPlay loop id="wakeUp">
+          <source src={WakeUp} type="video/mp4" />
+        </video>
+      );
+    }
+    return (
+      <video className="BGVideo" autoPlay muted loop id="backgroundVideo">
+        <source src={BackgroundVideo} type="video/mp4" />
+      </video>
+    );
+  };
+
+  const isAlarmTime = () => {
+    //these two lines give us our current time date to check in the if statement
+    const currentDate = new Date();
+
+    //these three lines of code give us the end time of our alarm for the if statement
+    const currentAlarmTime = new Date();
+    const alarmTimeFullDate = new Date(
+      currentAlarmTime.toDateString() + " " + alarmTime
+    );
+    const alarmEndTime = addMinutes(alarmTimeFullDate, 30);
+    console.log(differenceInMinutes(alarmEndTime, currentDate));
+
+    if (
+      alarmTime !== "" &&
+      differenceInMinutes(
+        setSeconds(alarmEndTime, 1),
+        setSeconds(currentDate, 1)
+      ) === 30
+    ) {
+      console.log("it is time");
+      return (
+        <video muted autoPlay loop id="wakeUp">
+          <source src={WakeUp} type="video/mp4" />
+        </video>
+      );
+    }
+    return null;
+  };
+
+  const alarmInputHandler = () => {
+    //these two lines give us our current time date to check in the if statement
+    const currentTime = new Date();
+    const currentTimeFullDate = new Date(
+      currentTime.toDateString() + " " + time
+    );
+
+    //these three lines of code give us the end time of our alarm for the if statement
+    const currentAlarmTime = new Date();
+    const alarmTimeFullDate = new Date(
+      currentAlarmTime.toDateString() + " " + alarmTime
+    );
+    const alarmEndTime = addMinutes(alarmTimeFullDate, 30);
+
+    console.log(alarmTimeFullDate);
+    console.log(currentAlarmTime);
+    console.log(alarmEndTime);
+  };
+
+  setInterval(updateTime, 1000);
+
+  return (
+    <div className="App">
+      {alarmClock()}
+      {isAlarmTime()}
+      <div className="time">{time}</div>
+      <p>What Are The Words That Changed Your Life?</p>
+      <label>
+        Enter Alarm
+        <input
+          onChange={(e) => setAlarmTime(e.target.value)}
+          type="time"
+        ></input>
+      </label>
+      <button onClick={alarmInputHandler}>submit</button>
+    </div>
+  );
+}
+
+export default App;

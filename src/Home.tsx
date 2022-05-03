c18fcd2e44096190b396122d13408d0470b71f36
crimtin <christianmck@hotmail.com>
2022-05-02 23:51:44 -0500

    added css to alram video and the input. instead of a singular bg video we now have an array that playes randomly. also eliminted old code. of course added new videos for background.

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,28 @@
-import BackgroundVideo from "./Assets/BackgroundVideo.mp4";
-import WakeUp from "./WakeUp.mp4";
+import BackgroundVideo from "./assets/BackgroundVideo.mp4";
+import CherryBG from "./assets/CherryBG.mp4";
+import ShoreBG from "./assets/ShoreBG.mp4";
+import VinlandBG from "./assets/VinlandBG.mp4";
+import SpaceCowboyBG from "./assets/SpaceCowboyBG.mp4";
+import JapaneseLake from "./assets/JapaneseLake.mp4";
+import RamenStall from "./assets/RamenStall.mp4";
 import { useState } from "react";
 import { addMinutes, isWithinInterval } from "date-fns";
 import { differenceInMinutes } from "date-fns";
+import "./Home.css";
 
 const Home = () => {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
   const [alarmTime, setAlarmTime] = useState("");
+
+  const backgroundVideos = [
+    BackgroundVideo,
+    CherryBG,
+    ShoreBG,
+    VinlandBG,
+    SpaceCowboyBG,
+    JapaneseLake,
+    RamenStall,
+  ];
 
   const updateTime = () => {
     const localeTime = new Date().toLocaleTimeString();
@@ -14,17 +30,18 @@ const Home = () => {
     setTime(localeTime);
   };
 
+  // this needs to be deleted and moved to the return code.
   const alarmClock = () => {
-    if (time === "2:47:0") {
-      return (
-        <video autoPlay loop id="wakeUp">
-          <source src={WakeUp} type="video/mp4" />
-        </video>
-      );
-    }
     return (
       <video className="BGVideo" autoPlay muted loop id="backgroundVideo">
-        <source src={BackgroundVideo} type="video/mp4" />
+        <source
+          src={
+            backgroundVideos[
+              Math.floor(Math.random() * backgroundVideos.length)
+            ]
+          }
+          type="video/mp4"
+        />
       </video>
     );
   };
@@ -49,21 +66,25 @@ const Home = () => {
     ) {
       console.log("it is time");
       return (
-        <video muted autoPlay loop id="wakeUp">
-          <source src={WakeUp} type="video/mp4" />
-        </video>
+        <div>
+          <iframe
+            className="lofiVideo"
+            src="https://www.youtube.com/embed/DWcJFNfaw9c?autoplay=1&controls=0"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        // <video muted autoPlay loop id="wakeUp">
+        //   <source src={WakeUp} type="video/mp4" />
+        // </video>
       );
     }
     return null;
   };
 
   const alarmInputHandler = () => {
-    //these two lines give us our current time date to check in the if statement
-    const currentTime = new Date();
-    const currentTimeFullDate = new Date(
-      currentTime.toDateString() + " " + time
-    );
-
     //these three lines of code give us the end time of our alarm for the if statement
     const currentAlarmTime = new Date();
     const alarmTimeFullDate = new Date(
@@ -86,6 +107,7 @@ const Home = () => {
       <label>
         Enter Alarm
         <input
+          className="timeInput"
           onChange={(e) => setAlarmTime(e.target.value)}
           type="time"
         ></input>

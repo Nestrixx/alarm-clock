8d8ed5ee6d3632af91780238ad5505ce3be06c99
crimtin <christianmck@hotmail.com>
2022-05-21 01:32:13 -0500

    added styling for alarmList. added more styling to alarm pages and a new button to return home.

--- src/AlarmPage/AlarmList.tsx
+++ src/AlarmPage/AlarmList.tsx
@@ -1,16 +1,18 @@
 import { TimeContext } from "../TimeContext.js";
 import { useContext } from "react";
 import { AlarmContext } from "../types/AlarmContext.js";
+import "./AlarmList.scss";
 
 const AlarmList = () => {
   const { alarms }: AlarmContext = useContext(TimeContext);
 
   if (alarms.length > 0) {
     return (
-      <ul>
+      <ul className="list-wrapper">
         {alarms.map((alarm) => (
-          <li key={alarm.name}>
+          <li key={alarm.name} id="alarm-list">
             {alarm.name}
+            {' '}
             {alarm.alarm}
           </li>
         ))}
--- src/AlarmPage/AlarmPage.tsx
+++ src/AlarmPage/AlarmPage.tsx
@@ -17,46 +17,51 @@ const AlarmPage = () => {
   };
 
   return (
-    <div className="monica">
-      <div className="time-button-wrapper">
-        <div className="alarmName-input">
-          <input
-            type="text"
-            placeholder="Enter Alarm Name"
-            onChange={(e) => setPendingAlarmName(e.target.value)}
-          ></input>
-        </div>
-        <div className="dateTime-input">
-          <input
-            className="timeInput"
-            type="datetime-local"
-            min={format(new Date(), "yyyy-MM-dd'T'HH:mm")}
-            onChange={(e) => setPendingAlarmTime(e.target.value)}
-          />
-        </div>
-        <div className="alarm-checkbox">
-          <input
-            type="checkbox"
-            onChange={(e) => setPendingIsRepeated(e.target.checked)}
-            id="repeat-checkbox"
-          ></input>
-          <label htmlFor="repeat-checkbox"> repeat this alarm?</label>
-        </div>
-        <div className="alarms-submit-button">
-          {/* <Link to="/"> */}
-          <button
-            className="submit-button"
-            onClick={() =>
-              addAlarm({
-                alarm: pendingAlarmTime,
-                name: pendingAlarmName,
-                isReoccurring: pendingIsRepeated,
-              })
-            }
-          >
-            Set Alarms
-          </button>
-          {/* </Link> */}
+    <div className="alarm-page-wrapper">
+      <div className="monica">
+        <div className="time-button-wrapper">
+          <div className="alarmName-input">
+            <input
+              type="text"
+              placeholder="Enter Alarm Name"
+              onChange={(e) => setPendingAlarmName(e.target.value)}
+            ></input>
+          </div>
+          <div className="dateTime-input">
+            <input
+              className="timeInput"
+              type="datetime-local"
+              min={format(new Date(), "yyyy-MM-dd'T'HH:mm")}
+              onChange={(e) => setPendingAlarmTime(e.target.value)}
+            />
+          </div>
+          <div className="alarm-checkbox">
+            <input
+              type="checkbox"
+              onChange={(e) => setPendingIsRepeated(e.target.checked)}
+              id="repeat-checkbox"
+            ></input>
+            <label htmlFor="repeat-checkbox"> repeat this alarm?</label>
+          </div>
+          <div className="alarms-submit-button">
+            {/* <Link to="/"> */}
+            <button
+              className="submit-button"
+              onClick={() =>
+                addAlarm({
+                  alarm: pendingAlarmTime,
+                  name: pendingAlarmName,
+                  isReoccurring: pendingIsRepeated,
+                })
+              }
+            >
+              Set Alarms
+            </button>
+            <Link to="/">
+              <button className="home-page-button">home page</button>
+            </Link>
+            {/* </Link> */}
+          </div>
         </div>
       </div>
       <AlarmList />

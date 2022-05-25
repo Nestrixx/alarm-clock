bffd21af0572e009a8bb90e07b729c3aa12fbc02
crimtin <christianmck@hotmail.com>
2022-05-24 23:19:54 -0500

    updated styles. handled alarm delete.

--- src/AlarmPage/AlarmList.tsx
+++ src/AlarmPage/AlarmList.tsx
@@ -3,23 +3,44 @@ import { useContext } from "react";
 import { AlarmContext } from "../types/AlarmContext.js";
 import "./AlarmList.scss";
 import format from "date-fns/format";
+import { IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 const AlarmList = () => {
-  const { alarms }: AlarmContext = useContext(TimeContext);
-  if (alarms.length > 0) {
-    return (
-      <div className="total-list-wrapper">
-        <ul className="ulist-wrapper">
-          {alarms.map((alarm) => (
-            <li key={alarm.name} id="alarm-list">
-              {alarm.name} {format(new Date(alarm.alarm), "Pp")}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+  const { alarms, setAlarms }: AlarmContext = useContext(TimeContext);
+  const deleteButtonHandler = (alarmIndex: number, alarmName: string) => {
+    window.localStorage.removeItem(alarmName);
+    setAlarms((oldAlarms) => {
+      const newAlarms = oldAlarms.filter((alarm) => {
+        console.log(alarm.name);
+        return alarm.name !== oldAlarms[alarmIndex].name;
+      });
+      return newAlarms;
+    });
+  };
+
+  if (alarms.length === 0) {
+    return null;
   }
 
-  return null;
+  return (
+    <div className="total-list-wrapper">
+      <ul className="ulist-wrapper">
+        {alarms.map((alarm, index) => (
+          <li key={alarm.name} className="alarm-list">
+            <div>
+              {alarm.name} {format(new Date(alarm.alarm), "Pp")}
+              <IconButton
+                onClick={() => deleteButtonHandler(index, alarm.name)}
+                aria-label="delete"
+              >
+                <CloseIcon />
+              </IconButton>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default AlarmList;
